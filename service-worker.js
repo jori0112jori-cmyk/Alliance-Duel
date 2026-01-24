50c0fc8dd61679688e98805731a98a8b5fe3a0b8
jori0112jori-cmyk <jori.0112.jori@gmail.com>
2026-01-24 13:37:42 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'duel-calc-v1';
+const CACHE_NAME = 'duel-calc-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -22,4 +22,5 @@ self.addEventListener('fetch', event => {
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
+
 });
